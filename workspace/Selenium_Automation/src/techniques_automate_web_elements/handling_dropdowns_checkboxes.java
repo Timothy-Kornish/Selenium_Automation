c67a2e430006ce4712a71c806d22548d4676a928
Timothy Kornish <TimmehK@Timothys-MacBook-Pro.local>
2017-12-19 21:30:39 -0700

    implicit vs explicit wait in selenium

--- workspace/Selenium_Automation/src/techniques_automate_web_elements/handling_dropdowns_checkboxes.java
+++ workspace/Selenium_Automation/src/techniques_automate_web_elements/handling_dropdowns_checkboxes.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 public class handling_dropdowns_checkboxes {
 
 	public static void main(String[] args) {
