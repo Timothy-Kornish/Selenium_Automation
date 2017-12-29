b8f479f65378f59041e12e4f20dc8b8f2c13ca0a
Timothy Kornish <TimmehK@Timothys-MacBook-Pro.local>
2017-12-28 20:58:05 -0700

    Adding jenkins files

---
+++ workspace/jenkins/src/jenkins/initialize_jenkins.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+public class initialize_jenkins {
+
+	/*
+	 * To initialize jenkins from the command line:
+	 * -navigate to directory with jenkins.war file
+	 * enter:
+	 * 		java -jar jenkins.war --httpPort=8888
+	 */
+}
